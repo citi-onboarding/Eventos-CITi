71790dcf776cbe693c2ea1c4da70bcc4ca4b4e9f
Lucas Monterazo <lsm6@cin.ufpe.br>
2020-11-27 20:00:02 -0300

    feat(create-blog-model): erase blank lines

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -16,26 +16,22 @@ BlogPost.add({
     type: Types.CloudinaryImage,
     required: true,
     initial: true,
-
   },
   date: {
     type: Types.Date,
     required: true,
     initial: true,
   },
-
   description: {
     type: Types.Text,
     required: true,
     initial: true,
   },
-
   link: {
     type: Types.Url,
     required: true,
     initial: true,
   },
-  
 });
 
 BlogPost.register();
