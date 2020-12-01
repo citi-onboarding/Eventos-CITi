ad49c2b25d32e1e1adfcfb89a52eb9e487159865
Lucas Monterazo <lsm6@cin.ufpe.br>
2020-12-01 17:48:34 -0300

    feat(create-why-we-do-it-on-client): fix client error)

--- client/src/components/WhyWeDoIt/WhyWeDoIt.jsx
+++ client/src/components/WhyWeDoIt/WhyWeDoIt.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+import './style.css';
+
+
 
 export default function WhyWeDoIt () {
-  return 
-};
+  const [why_we_do_it, setWhyWeDoIt] = useState([]);
+
+  const loadWhyWeDoIt = async () => {
+    const res = await axios.get('http://localhost:3001/admin/why-we-do-its');
+    setWhyWeDoIt(res.data);
+  };
+
+  useEffect(() => {
+    loadWhyWeDoIt();
+  }, []);
+
+  return (
+    <section className="why-we-do-it">
+      {why_we_do_it?.map(({ _id, title, image, description }) => (
+        <div key={_id} className="post-card">
+          <h2>{title}</h2>
+          <img src={image[0]?.url} alt="Post de Exemplo"/>
+          <p>{description}</p>
+        </div>
+      ))}
+    </section>
+  );
+}
