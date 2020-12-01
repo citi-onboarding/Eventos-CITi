d6eb4a148aa3f81e9d5ee64b74d4c3ff7377cefc
guilherme <gfxs@cin.ufpe.br>
2020-12-01 18:50:51 -0300

    feat(create-footer): create initial divs

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+
+  return (
+    <footer>
+        <div className="contatoFooter">contato</div>
+        <div className="logo">logo</div>
+        <div className="endereco">endereco e made with</div>
+    </footer>
+  );
+}
+
+export default Footer;
