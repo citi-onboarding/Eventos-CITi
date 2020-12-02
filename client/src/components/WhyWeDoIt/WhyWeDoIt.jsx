142ce55fc1f60af5ddb6b31ce936f2c9b3b779ea
Lucas Monterazo <lsm6@cin.ufpe.br>
2020-12-01 21:02:29 -0300

    feat(create-why-we-do-it-on-client): fix return error

--- client/src/components/WhyWeDoIt/WhyWeDoIt.jsx
+++ client/src/components/WhyWeDoIt/WhyWeDoIt.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import './styles.css';
 
 export default function WhyWeDoIt () {
-  const [whyWeDoIt, setWhyWeDoIt] = useState(['teste']);
+  const [whyWeDoIt, setWhyWeDoIt] = useState();
 
   const loadWhyWeDoIt = async () => {
     const res = await axios.get('http://localhost:3001/api/why-we-do-it');
@@ -19,11 +19,11 @@ export default function WhyWeDoIt () {
 
   return (
     <section className="why-we-do-it">
-      <h2>{whyWeDoIt[0].title}</h2>
-      <img src="https://s2.glbimg.com/zPloOmW9yoEcYyMiYcdIHhpet5s=/780x440/e.glbimg.com/og/ed/f/original/2015/03/18/tilapia_1.jpg" alt="Placeholder"/>
-      <p>{whyWeDoIt[0].description}</p>
+      <h2>{whyWeDoIt && whyWeDoIt[0].title}</h2>
+      <img src={whyWeDoIt && whyWeDoIt[0].image.secure_url} alt="Image"/>
+      <p>{whyWeDoIt && whyWeDoIt[0].description}</p>
     </section>
   );
 }
 
-// Scala tinha pedido pra usar <p>{whyWeDoIt[0].description}</p> mas a ? bugava p javascript
+// Scala tinha pedido pra usar <p>{whyWeDoIt?[0].description}</p> mas a ? bugava p javascript
