4e0341d96728fecf69e98efe0337fe949e538156
Lucas Monterazo <lsm6@cin.ufpe.br>
2020-12-05 12:19:41 -0300

    feat(create-blog-on-client): add style to blogcard

--- client/src/components/BlogCard/BlogCard.jsx
+++ client/src/components/BlogCard/BlogCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import './style.css' 
+import './style.css'
 export default function Blog () {
   const [blog, setBlog] = useState();
   const loadBlog = async () => {
@@ -15,11 +15,11 @@ export default function Blog () {
   // Código HTML
   return (
     <article className="BlogCard" id="blogCard">
-      <img src="https://capitalhotelschool.co.za/wp-content/uploads/2017/09/news-placeholder.png" alt="Placeholder"/>
+      <img src="https://capitalhotelschool.co.za/wp-content/uploads/2017/09/news-placeholder.png" alt="Placeholder" className="BlogCardImage" />
       <div className="content">
-        <p>Eventos / 26 de nevembro de 2020</p>
+        <p className="Date">Eventos / 26 de nevembro de 2020</p>
         <h3>O Tech Center foi muito legal</h3>
-        <p>Nossa foi muito bom mesmo eu estava lá e vi tudo com os meus olhos. Tinha muitas pessoas inteligentes e legais, nossa como eu amei...</p>
+        <p className="Description">Nossa foi muito bom mesmo eu estava lá e vi tudo com os meus olhos. Tinha muitas pessoas inteligentes e legais, nossa como eu amei...</p>
         <a href="#">Leia o texto completo</a>
       </div>
     </article>
