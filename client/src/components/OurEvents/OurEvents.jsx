875349e4f9e0b0127250cc4708bf3ec552024380
Guilherme <gfxs@cin.ufpe.br>
2020-12-07 16:19:40 -0300

    feat(create-our-events-section): create inicial version of the section

---
+++ client/src/components/OurEvents/OurEvents.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import './styles.css';
+
+import { useState, useEffect } from 'react';
+
+function OurEvents() {
+
+    const settings = {
+        className: "center",
+        centerMode: true,
+        infinite: true,
+        centerPadding: "60px",
+        slidesToShow: 1,
+        speed: 500
+      };
+      
+    
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default OurEvents;
