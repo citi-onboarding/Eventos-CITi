345381d30f417f7d11ee356c266a2f611f240097
Guilherme <gfxs@cin.ufpe.br>
2020-12-09 00:23:53 -0300

    feat(create-our-events-section): create card stylization

--- client/src/components/OurEventsCard/OurEventsCard.jsx
+++ client/src/components/OurEventsCard/OurEventsCard.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './styles.css';
 import { useState, useEffect } from 'react';
+import arrow from './images/arrow.svg'
+import calendar from './images/calendar.svg'
+import gps from './images/gps.svg'
 function OurEventsCard(props) {
     var date = ""
     var eventDate = new Date(props.eventDate)
@@ -45,16 +48,25 @@ function OurEventsCard(props) {
             month = "Dezembro"
             break
     }
-    date = "Última edição em "+eventDate.getDay()+" de " + month +" de "+ eventDate.getFullYear()
+    date = "Última edição em " + eventDate.getDay() + " de " + month + " de " + eventDate.getFullYear()
 
     return (
         <div className="ourEventsCard">
             <img src={props.secureUrl}></img>
             <h3>{props.name}</h3>
-            <p className="date-and-local">{date}</p>
-            <p className="date-and-local">{props.local}</p>
-            <p>{props.description}</p>
-            <a href={props.knowMoreLink}>Inscreva-se</a>
+            <div className="dateAndLocal">
+                
+                <p><img src={calendar} alt="icone de calendario" />{date}</p>
+            </div>
+            <div className="dateAndLocal">
+                
+                <p><img src={gps} alt="icone de gps" />{props.local}</p>
+            </div>
+            <p className="description">{props.description}</p>
+            <div className="knowMore">
+                <a href={props.knowMoreLink} target="_blank" >Saiba mais</a>
+                <img src={arrow} alt="flecha apontando para o link" />
+            </div>
         </div>
     );
 }
