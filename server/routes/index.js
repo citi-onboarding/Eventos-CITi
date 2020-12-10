679bcdb18b5c83cd429d766ce2dc667258599794
Guilherme <gfxs@cin.ufpe.br>
2020-12-10 00:57:16 -0300

    feat(create-subscription-modal): create http post route to send email

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,65 +12,83 @@ const Mission = keystone.list('WhyWeDoIt');
 const Banner = keystone.list('Banner');
 
 module.exports = (app) => {
-  app.use(cors());
+    app.use(cors());
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-
-  app.get('/api/posts', (req, res) => {
-    Post.model.find((err, data) => {
-      if (err) {
-        res.status(500).send('DB Error');
-      } else {
-        res.send(data);
-      }
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
-  });
-  
-  app.get('/api/events', (req, res) => {
-    Events.model.find((err, data) => {
-      if (err) {
-        res.status(500).send('DB Error');
-      } else {
-        res.send(data);
-      }
+
+    app.get('/api/posts', (req, res) => {
+        Post.model.find((err, data) => {
+            if (err) {
+                res.status(500).send('DB Error');
+            } else {
+                res.send(data);
+            }
+        });
     });
-  });
 
-  app.get('/api/blogpost', (req, res) => {
-    BlogPost.model.find((err, data) => {
-      if (err) {
-        res.status(500).send('DB Error');
-      } else {
-        res.send(data);
-      }
+    app.get('/api/events', (req, res) => {
+        Events.model.find((err, data) => {
+            if (err) {
+                res.status(500).send('DB Error');
+            } else {
+                res.send(data);
+            }
+        });
     });
-  });
 
-
-
-  app.get('/api/why-we-do-it', (req, res) => {
-    Mission.model.find((err, data) => {
-      if (err) {
-        res.status(500).send('DB Error');
-      } else {
-        res.send(data);
-      }
+    app.get('/api/blogpost', (req, res) => {
+        BlogPost.model.find((err, data) => {
+            if (err) {
+                res.status(500).send('DB Error');
+            } else {
+                res.send(data);
+            }
+        });
     });
-  });
 
-  app.get('/api/banner', (req, res) => {
-    Banner.model.find((err, data) => {
-      if (err) {
-        res.status(500).send('DB Error');
-      } else {
-        res.send(data);
-      }
+
+
+    app.get('/api/why-we-do-it', (req, res) => {
+        Mission.model.find((err, data) => {
+            if (err) {
+                res.status(500).send('DB Error');
+            } else {
+                res.send(data);
+            }
+        });
     });
-  });
 
-  app.get('*', (req, res) => {
-		res.redirect('/');
-	});
+    app.get('/api/banner', (req, res) => {
+        Banner.model.find((err, data) => {
+            if (err) {
+                res.status(500).send('DB Error');
+            } else {
+                res.send(data);
+            }
+        });
+    });
+
+    app.post('/api/subscription', async(req, res) => {
+        try {
+            const { name, email, subject, message } = req.body;
+
+            const body = `
+        Nome: ${name}
+        Email: ${email}
+        Mensagem: ${message}
+      `;
+
+
+            res.status(200).send('Everything went good');
+        } catch (error) {
+            res.status(500).send('Something went wrong');
+            console.log(error);
+        }
+    });
+
+    app.get('*', (req, res) => {
+        res.redirect('/');
+    });
 };
