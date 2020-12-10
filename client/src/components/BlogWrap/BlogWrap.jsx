b7d3cd3b3c3b6eadd3a5a25a3058c51947071b49
Lucas Monterazo <lsm6@cin.ufpe.br>
2020-12-09 21:08:35 -0300

    feat(create-blog-on-client): add border to the images

--- client/src/components/BlogWrap/BlogWrap.jsx
+++ client/src/components/BlogWrap/BlogWrap.jsx
@@ -5,6 +5,51 @@ import '../BlogSlicker/BlogSlicker'
 
 export default function BlogWrap (props) {
   console.log(props.element) 
+  
+  var date = ""
+  var eventDate = new Date(props.element[0].name)
+  var month = ""
+  console.log(eventDate.getMonth())
+  switch (eventDate.getMonth()) {
+      case 1:
+          month = "Janeiro"
+          break;
+      case 2:
+          month = "Fevereiro"
+          break;
+      case 3:
+          month = "Março"
+          break
+      case 4:
+          month = "Abril"
+          break
+      case 5:
+          month = "Maio"
+          break
+      case 6:
+          month = "Junho"
+          break;
+      case 7:
+          month = "Julho"
+          break;
+      case 8:
+          month = "Agosto"
+          break
+      case 9:
+          month = "Setembro"
+          break
+      case 10:
+          month = "Outubro"
+          break
+      case 11:
+          console.log("entrou")
+          month = "Novembro"
+          break
+      case 12:
+          month = "Dezembro"
+          break
+  }
+  date = "Última edição em " + eventDate.getDay() + " de " + month + " de " + eventDate.getFullYear()
   function RenderCards (){
     if (props.element && props.element[0].name) {
       return(
