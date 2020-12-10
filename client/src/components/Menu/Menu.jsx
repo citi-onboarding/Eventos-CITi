76f1c636ba7437ea7f7cf61b44f7df6c00fc63dd
Lucas Monterazo <lsm6@cin.ufpe.br>
2020-12-10 20:52:21 -0300

    feat(create-menu): add link to the social menu buttons

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -20,12 +20,15 @@ export default function Menu() {
   return (
     <nav className="menu">
       <div className="container">
-         <CITiEventosLogo id="CITiLogo"/>
-        <ul className="socialMenu">
-          <li><FacebookIcon/></li>
-          <li><LinkdInIcon/></li>
-          <li><InstagramIcon/></li>
+        <CITiEventosLogo id="CITiLogo"/>
+        <div className="buttonsWrap">
+          <a className="menuButton"  onClick={handleClick}>AAAAAAAAAAAAAA</a>
+          <ul className="socialMenu">
+          <li><a href="https://www.facebook.com/citiufpe/"><FacebookIcon/></a></li>
+          <li><a href="https://www.linkedin.com/school/cinufpe/"><LinkdInIcon/></a></li>
+          <li><a href="https://www.instagram.com/citiufpe/?hl=pt-br"><InstagramIcon/></a></li>
         </ul>
+        </div>
         <ul className="sectionsMenu">
           <li><a href="#Banner">Início</a></li>
           <li><a href="#sectionWhyWeDoIt">Por que fazemos?</a></li>
@@ -34,7 +37,7 @@ export default function Menu() {
           <li><a href="#GetToKnowOurBlog">Conheça nosso blog</a></li>
           <li><a href="#Contact">Contato</a></li>
         </ul>
-        <a className="menuButton"  onClick={handleClick}>AAAAAAAAAAAAAA</a>
+
       </div>
     </nav> 
   );
