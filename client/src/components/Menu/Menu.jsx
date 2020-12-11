89e29a3be0561f19421d7d3a2ddd166836da4cb2
Lucas Monterazo <lsm6@cin.ufpe.br>
2020-12-11 00:42:47 -0300

    feat(create-menu): fix mobile menu

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './style.css';
 
+import {ReactComponent as BubbleBurguer} from './BubbleBurguer.svg';
+import {ReactComponent as BurguerOpen} from './BurguerOpen.svg';
+import {ReactComponent as BurguerFrame} from './BurguerClosed.svg';
 import {ReactComponent as CITiEventosLogo} from './CITiEventosLogo.svg';
 import {ReactComponent as FacebookIcon} from './FacebookIcon.svg';
 import {ReactComponent as LinkdInIcon} from './LinkdInIcon.svg';
@@ -8,8 +11,6 @@ import {ReactComponent as InstagramIcon} from './InstagramIcon.svg';
 
 
 export default function Menu() {
-  let menuButton = "yyyyyyyyy";
-
   function handleClick() {
     const renderMenu = document.querySelector(".menu")
     if (renderMenu){
@@ -18,35 +19,34 @@ export default function Menu() {
   }
 
   return (
+    <div>
+      <div className="alignment"></div>
     <nav className="menu">
       <div className="container">
-        <CITiEventosLogo id="CITiLogo"/>
+        <CITiEventosLogo id="CitiLogo"/>
         <div className="buttonsWrap">
-          <a className="menuButton"  onClick={handleClick}>AAAAAAAAAAAAAA</a>
           <ul className="socialMenu">
-          <li><a href="https://www.facebook.com/citiufpe/"><FacebookIcon/></a></li>
-          <li><a href="https://www.linkedin.com/school/cinufpe/"><LinkdInIcon/></a></li>
-          <li><a href="https://www.instagram.com/citiufpe/?hl=pt-br"><InstagramIcon/></a></li>
+          <li><a href="https://www.facebook.com/citiufpe/"><FacebookIcon id="FacebookIcon"/></a></li>
+          <li><a href="https://www.linkedin.com/school/cinufpe/"><LinkdInIcon id="LinkdInIcon"/></a></li>
+          <li><a href="https://www.instagram.com/citiufpe/?hl=pt-br"><InstagramIcon id="InstagramIcon"/></a></li>
         </ul>
+          <div className="menuButton" onClick={handleClick}>
+            <BurguerFrame id="burguerClosed"/>
+            </div>
+          </div>  
+          <div className="burguerButton" >
+          <BurguerOpen id="burguerOpen" onClick={handleClick}/> 
         </div>
         <ul className="sectionsMenu">
           <li><a href="#Banner" onClick={handleClick}>Início</a></li>
           <li><a href="#sectionWhyWeDoIt" onClick={handleClick}>Por que fazemos?</a></li>
-          <li><a href="" onClick={handleClick}>Agenda</a></li>
-          <li><a href="" onClick={handleClick}>Outros Eventos</a></li>
+          <li><a href="" onClick={handleClick}>Eventos</a></li>
           <li><a href="#GetToKnowOurBlog" onClick={handleClick}>Conheça nosso blog</a></li>
           <li><a href="#Contact" onClick={handleClick}>Contato</a></li>
         </ul>
-
       </div>
-    </nav> 
+        <BubbleBurguer id="BurguerBubble"/>
+    </nav>
+    </div> 
   );
 }
-
-
-
-// function handleClick() {
-//   button.classList.toggle("change")
-// }
-
-// onClick={handleClick}
