14ece176f313789eab4dbf61d830672e3e59e686
Lucas Monterazo <lsm6@cin.ufpe.br>
2020-12-12 00:57:49 -0300

    feat(create-contact-section): add responsiveness

--- client/src/components/ContactSection/ContactSection.jsx
+++ client/src/components/ContactSection/ContactSection.jsx
@@ -2,13 +2,10 @@ import './styles.css';
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import bolha from './images/bolha.svg';
-import linkedin from './images/linkedin.svg'
-import facebook from './images/facebook.svg'
-import instagram from './images/facebook.svg'
+import {ReactComponent as LinkedIn} from  './images/linkedin.svg';
+import {ReactComponent as Facebook} from  './images/facebook.svg';
+import {ReactComponent as Instagram} from './images/insta.svg';
 function ContactSection(props) {
-
-
-
     //code with states to be put by fomr 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
@@ -31,13 +28,14 @@ function ContactSection(props) {
             console.log(err?.response || err);
         }
     };
-
+    
     return (
         <section id="contact" className="contact-place">
             <div className="contact-content">
+                <img src={bolha} alt="imagem decorativa" id="BubbleContact"/>
                 <form onSubmit={handleSubmit}>
-                    <h2>Fale conosco</h2>
                     <div className="form" />
+                    <h2>Fale conosco</h2>
                     <p>Nome</p>
                     <input
                         required
@@ -66,14 +64,14 @@ function ContactSection(props) {
                         value={message}
                         onChange={(e) => setMessage(e.target.value)}
                     />
-                    <input type="submit" value="Confirmar inscrição" />
+                    <input type="submit" value="Enviar" id="confirmSubscription" />
                 </form>
-
-                <div className="social-network-contact">
-                    <p><img src={instagram}/>@citiufpe</p>
-                    <p>/<img src={linkedin}/>company/citiufpe</p>
-                    <p><img src={facebook}/>/citiufpe/</p>
-                    <img src={bolha} alt="imagem decorativa"/>
+                <div className="information">
+                    <ul>
+                        <li><a href=""><Instagram className="socialIcon"/> @citiufpe</a></li>
+                        <li><a href=""><LinkedIn className="socialIcon"/> /company/citiufpe</a></li>
+                        <li><a href=""><Facebook className="socialIcon"/> /citiufpe/</a></li>
+                    </ul>
                 </div>
             </div>
         </section>
